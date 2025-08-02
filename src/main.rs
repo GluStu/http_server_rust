010785f28ea50af1c7fede80ca4f8a8e633ac64f
GluStu <sanchitbokade8@gmail.com>
2025-08-02 19:45:08 +0530

    test binding to any avail port

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::io::Result;
 use rust_mail::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<()>{
-    run()?.await
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener).await?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,9 +1,23 @@
+use std::net::TcpListener;
+use reqwest::Client;
+
 #[tokio::test]
 async fn  health_check_test() {
-    let _ = spawn_app();
+    let address: String = spawn_app().await;
+    let client = Client::new();
+    let response = client
+                                    .get(&format!("{}/health_check", &address))
+                                    .send()
+                                    .await
+                                    .expect("req exec fail");
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
 }
 
-async fn spawn_app(){
-    let server = rust_mail::run().expect("failed to bind");
+async fn spawn_app() -> String {
+    let listner = TcpListener::bind("127.0.0.1:0").expect("binding failed");
+    let port = listner.local_addr().unwrap().port();
+    let server = rust_mail::run(listner).await.expect("failed to bind");
     let _ = tokio::spawn(server);
+    return format!("http://127.0.0.1:{}", port);
 }
